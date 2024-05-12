6c5223157aff749a30c737a2d41516c71689e800
Batuhan Ayaz SCHOOL <batuhan.ayaz@student.ehb.be>
2024-05-12 15:49:44 +0200

    Icons added + Stack navigation added

--- App/Navigations/TabNavigation.js
+++ App/Navigations/TabNavigation.js
@@ -6,14 +6,51 @@ import MyCourse from "../Screen/MyCourse";
 import Profile from "../Screen/Profile";
 import LeaderBoard from "../Screen/LeaderBoard";
 
+//ICONS
+import AntDesign from "@expo/vector-icons/AntDesign";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { MaterialIcons } from "@expo/vector-icons";
+
 const Tab = createBottomTabNavigator();
 export default function TabNavigation() {
   return (
     <Tab.Navigator screenOptions={{ headerShown: false }}>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Mijn Cursus" component={MyCourse} />
-      <Tab.Screen name="Profile" component={Profile} />
-      <Tab.Screen name="Scorebord" component={LeaderBoard} />
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Mijn Cursus"
+        component={MyCourse}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="book" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Scorebord"
+        component={LeaderBoard}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="sports-score" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="user-circle-o" size={size} color={color} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
